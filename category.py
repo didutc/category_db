397e112fe5e2f00a567cc4dd441e53e18ab1f9ba
didutc <didutc@gmail.com>
2022-05-06 12:44:16 +0900

    finish

--- category.py
+++ category.py
@@ -17,7 +17,9 @@ class category:  # 카테고리에서 서브 카테고리 없으면 출력 불�
 
 
     def Run(self):  # 파일 실행 함수
+        
         for li in range(1,10):
+            filename_list = str(li)
             name_list_list, id_list_list = self.Scoutcategory(filename_list)
             reverseCategory_list = self.Reversecategory(filename_list)
 
